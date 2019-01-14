2bfa5058406abee172748453fe0afaa4f1fdcaf6
chiyouhen <chiyouhen@gmail.com>
2019-01-14 12:09:47 +0800

    update

--- certverify.py
+++ certverify.py
@@ -9,6 +9,8 @@ import datetime
 import pyasn1.codec.der.decoder
 import pyasn1.codec.native.encoder
 import pyasn1_modules.rfc2459
+import logging
+logger = logging.getLogger('certverify')
 
 class CertVerify:
     def __init__(self):
@@ -19,9 +21,10 @@ class CertVerify:
         self.certificate_request_filepath = None
         self.certificate_request = None
         self.ssl_context = None
-        self.logger = logging.getLogger('certverify')
         self.results = []
         self.argparser = argparse.ArgumentParser()
+        self.argparser.add_argument('-o', '--output', help='output style', choices=['check', 'renew', 'add-domain'], default='check')
+        self.argparser.add_argument('--new-domains', dest='new_domains', nargs='+')
         self.argparser.add_argument('certificate_filepath')
         self.argparser.add_argument('privatekey_filepath')
         self.argparser.add_argument('certificate_request_filepath')
@@ -30,14 +33,16 @@ class CertVerify:
         h = logging.StreamHandler(sys.stderr)
         fmtr = logging.Formatter('[%(asctime)s] %(levelname)-8s %(filename)s:%(lineno)d %(funcName)s - %(message)s')
         h.setFormatter(fmtr)
-        self.logger.addHandler(h)
-        self.logger.setLevel(logging.DEBUG)
+        logger.addHandler(h)
+        logger.setLevel(logging.DEBUG)
 
     def parse_args(self):
         args = self.argparser.parse_args()
         self.certificate_filepath = args.certificate_filepath
         self.certificate_request_filepath = args.certificate_request_filepath
         self.privatekey_filepath = args.privatekey_filepath
+        self.output_style = args.output
+        self.new_domains = args.new_domains
 
     def load_files(self):
         self.certificate = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, open(self.certificate_filepath, 'r').read())
@@ -86,7 +91,48 @@ class CertVerify:
         alt_names = [i['dNSName'].decode('utf-8') for i in alt_name_lines]
         return self.sort_hostnames(alt_names)
 
-    def output(self):
+    def get_alt_names(self):
+        ret = []
+        for i in range(0, self.certificate.get_extension_count()):
+            ext = self.certificate.get_extension(i)
+            if ext.get_short_name() == b'subjectAltName':
+                alt_names = self.decode_subject_alt_names(ext.get_data())
+                ret += alt_names
+        return ret
+
+    def get_noafter(self):
+        return datetime.datetime.strptime(self.certificate.get_notAfter().decode('utf-8'), '%Y%m%d%H%M%SZ')
+
+    def get_nobefore(self):
+        return datetime.datetime.strptime(self.certificate.get_notBefore().decode('utf-8'), '%Y%m%d%H%M%SZ')
+
+    def output_renew(self):
+        subject = self.certificate.get_subject()
+        print(f'Organization Name: {subject.organizationName}')
+        print(f'Country Name: {subject.countryName}')
+        print(f'State or Province Name: {subject.stateOrProvinceName}')
+        print(f'Locality Name: {subject.localityName}')
+
+        notafter = self.get_noafter()
+        print(f'Not After: {notafter}')
+        notbefore = self.get_nobefore()
+        print(f'Not Before: {notbefore}')
+
+        common_name = subject.commonName
+        alt_names = self.get_alt_names()
+        if self.output_style == 'add-domain':
+            for h in self.new_domains:
+                if h in alt_names:
+                    raise ValueError(f'domain {h} is already in this certificate')
+            alt_names += self.new_domains
+        alt_names.remove(common_name)
+        
+        print(f'Common Name: {subject.commonName}')
+        print(f'Alternative Names: {";".join(alt_names)}')
+        if self.output_style == 'add-domain':
+            print(f'New Names: {";".join(self.new_domains)}')
+
+    def output_check(self):
         print('Summary:')
         print('Subject:')
         subject = self.certificate.get_subject()
@@ -98,23 +144,27 @@ class CertVerify:
             print(f'{k.decode("utf-8")}: {v.decode("utf-8")}')
 
         print('Extensions:')
-        for i in range(0, self.certificate.get_extension_count()):
-            ext = self.certificate.get_extension(i)
-            if ext.get_short_name() == b'subjectAltName':
-                print('SubjectAltNames:')
-                alt_names = self.decode_subject_alt_names(ext.get_data())
-                max_width = max([len(h) for h in alt_names])
-                for h in alt_names:
-                    print(f'{h:>{4+max_width}}')
+        alt_names = self.get_alt_names()
+        max_width = 0
+        for h in alt_names:
+            if len(h) > max_width:
+                max_width = len(h)
+        for h in alt_names:
+            print(f'{h:>{4+max_width}}')
 
-        notafter = datetime.datetime.strptime(self.certificate.get_notAfter().decode('utf-8'), '%Y%m%d%H%M%SZ')
+        notafter = self.get_noafter()
         print(f'Not After: {notafter}')
-        notbefore = datetime.datetime.strptime(self.certificate.get_notBefore().decode('utf-8'), '%Y%m%d%H%M%SZ')
+        notbefore = self.get_nobefore()
         print(f'Not Before: {notbefore}')
         print('Check Results:')
         for r in self.results:
             print(f'{r[0]}: {r[1]}, {r[2]}')
 
+    def output(self):
+        if self.output_style == 'check':
+            self.output_check()
+        elif self.output_style in ['renew', 'add-domain']:
+            self.output_renew()
 
     def main(self):
         try:
@@ -127,7 +177,7 @@ class CertVerify:
             self.output()
             return 0
         except Exception as e:
-            print(traceback.format_exc())
+            logger.error(f'Exception {e} occured', exc_info=sys.exc_info())
             return 1
 
 if __name__ == '__main__':
